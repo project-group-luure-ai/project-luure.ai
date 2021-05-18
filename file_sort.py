e67195cf7f79adeed34271425e063b83fa72f8c9
sreeharijp <44796225+sreeharijp@users.noreply.github.com>
2021-05-18 13:23:29 +0530

    file_sort.py
    Fully functional shell based file sorter

--- file_sort.py
+++ file_sort.py
@@ -2,45 +2,47 @@ import os
 import glob
 import shutil
 from os import path
-filename=glob.glob("C:/Users/HP/Downloads/*")
-documents=['.pdf','.docx','.doc','.txt']
-media=['.jpeg','.jpg','.svg','.png','.PNG','.mp4','.mp3']
-setupFiles=['.exe','.msi']
-compressedFiles=['.zip']
-files=['.apk']
-DocumentsLocation='C:/Users/HP/Downloads/documents'
-mediaLocation='C:/Users/HP/Downloads/media'
-setupFilesLocation='C:/Users/HP/Downloads/setupFiles'
-compressedFilesLocation='C:/Users/HP/Downloads/compressedFiles'
-FilesLocation='C:/Users/HP/Downloads/Files'
+import sys
+print("Welcome to file sort\nEnter 0 in any prompt to exit\nBasic ideas of files and file extensions are necessary for operation of the program")
+status='not ok'
+while(status=='not ok'):
+    in_loc=str(input("Enter the location to be sorted:"))+"\*"
+    if in_loc =='0\*':
+        sys.exit("Termination!!!")
+    filename=glob.glob(in_loc)
+    status='ok'
+    if not filename:
+        status='not ok'
+        print("The folder doesn't exist!!Check the input and try again...\n0 for termination")
+files=[]
+FilesLocation=str(input("Enter the location to store the sorted files:"))
+if FilesLocation=='0':
+    sys.exit("Termination!!!")
+status='continue'
+while(status=='continue'):
+    status='stop'
+    exten=input("Enter the extention of the files to be placed in selected location:")
+    if exten=='0':
+        sys.quit("Termination!!!")
+    files.append(str(exten))
+    var=input("Do you want to enter more(0:No)(1:Yes)")
+    if var==1:
+        status='continue'
 for file in filename:
-    if os.path.splitext(file)[1] in documents:
-        if(path.exists(DocumentsLocation)):
-            shutil.move(file,DocumentsLocation)
-        else:
-            os.mkdir(DocumentsLocation)
-            shutil.move(file,DocumentsLocation)
-    if os.path.splitext(file)[1] in media:
-        if(path.exists(mediaLocation)):
-            shutil.move(file,mediaLocation)
-        else:
-            os.mkdir(mediaLocation)
-            shutil.move(file,mediaLocation)
-    if os.path.splitext(file)[1] in setupFiles:
-        if(path.exists(setupFilesLocation)):
-            shutil.move(file,setupFilesLocation)
-        else:
-            os.mkdir(setupFilesLocation)
-            shutil.move(file,setupFilesLocation)
-    if os.path.splitext(file)[1] in compressedFiles:
-        if(path.exists(compressedFilesLocation)):
-            shutil.move(file,compressedFilesLocation)
-        else:
-            os.mkdir(compressedFilesLocation)
-            shutil.move(file,compressedFilesLocation)
+    
     if os.path.splitext(file)[1] in files:
+        print("moving...")
         if(path.exists(FilesLocation)):
             shutil.move(file,FilesLocation)
         else:
-            os.mkdir(FilesLocation)
-            shutil.move(file,FilesLocation)
+            status='error'
+            while(status=='error'):
+                try:
+                    os.mkdir(FilesLocation)
+                    shutil.move(file,FilesLocation)
+                except OSError as error:
+                    print(error)
+                    FilesLocation=input("Enter a different location:(0 for termination)")
+                    if FilesLocation=='0':
+                        sys.exit("Termination!!!")
+                    status='error'

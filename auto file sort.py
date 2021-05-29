473c9a5255c55f69ad477563fe107ccb2d5bc117
sreeharijp <44796225+sreeharijp@users.noreply.github.com>
2021-05-29 20:09:36 +0530

    Update auto file sort.py

--- auto file sort.py
+++ auto file sort.py
@@ -1,13 +1,12 @@
 import os
-import glob
 import shutil
-from os import path
 import sys
+
 print("Welcome to file sort\nEnter 0 in any prompt to exit\nBasic ideas of files and file extensions are necessary for operation of the program")
 status='not ok'
 while(status=='not ok'):
-    directory=str(input("Enter the location to be sorted:"))+"\\"
-    if directory =='0\\':
+    directory=str(input("Enter the location to be sorted:"))+os.sep
+    if directory ==("0"+os.sep):
         sys.exit("Termination!!!")
     filename=os.listdir(directory)
     status='ok'
@@ -21,7 +20,7 @@ for (dirpath, dirnames, filenames) in os.walk(directory):
     break
 for i in range(len(filenames)):
     name,exten=os.path.splitext(filenames[i])
-    if(path.exists(directory+exten[1:])):
+    if(os.path.exists(directory+exten[1:])):
             shutil.move(directory+filenames[i],directory+exten[1:])
     else:
         os.mkdir(directory+exten[1:])
